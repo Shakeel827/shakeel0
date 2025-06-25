dd0fddd4f4617c24b53ccdc9b3a6cf9a53906ce6
Shakeel827 <skshakeel9086@gmail.com>
2025-06-25 21:58:05 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   optimizeDeps: {
     include: ['lucide-react', 'jspdf'], // Add any other non-ESM dependencies here
